29de1f89eab6ed510342731eb04a4cc9d3010036
omidgilan <63928957+omidgilan@users.noreply.github.com>
2025-10-25 03:30:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,14 @@
 import telebot
 
-# توکن ربات
+# توکن ربات رو اینجا بذار
 TOKEN = "5548149661:AAFblu4NL86utR9SbzuE6RQ27HuD3Uiynas"
 bot = telebot.TeleBot(TOKEN)
 
-# دستور /start
+# وقتی کاربر /start رو ارسال کرد
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "سلام! ربات آنلاین است و آماده کار کردن.")
+    bot.reply_to(message, "سلام! ربات تستی فعال شد ✅")
 
-# پاسخ ساده به پیام‌ها
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.send_message(message.chat.id, "پیام دریافت شد: " + message.text)
-
-# اجرای ربات
+# فعال کردن ربات (Polling)
+print("Bot is polling...")  # فقط برای اطلاع توی ترمینال
 bot.polling()
